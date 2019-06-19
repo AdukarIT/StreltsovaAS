08cd03aa124d2c2454928364c2c195afdf007fad
StreltsovaAS <ru-tyan@tut.by>
2019-06-19 19:17:16 +0300

    homework

--- Task-23/homework.js
+++ Task-23/homework.js
@@ -14,83 +14,83 @@ function getRandomArray(len) {
 
 	// Задание№2
 
-let len=prompt("Введите длину массива");
-let ArrayNumber=[];
+// let len=prompt("Введите длину массива");
+// let ArrayNumber=[];
 
 
-function getRandomArray(len) {
-	for(let i=0; i<len; i++)
-		ArrayNumber[i]=Math.round(Math.random()*100);
-	return ArrayNumber;
-}
+// function getRandomArray(len) {
+// 	for(let i=0; i<len; i++)
+// 		ArrayNumber[i]=Math.round(Math.random()*100);
+// 	return ArrayNumber;
+// }
 
-	getRandomArray(len);
+// 	getRandomArray(len);
 
-function ArithmeticalMean(sum, len) {
-	let sum = 0; 
-	sum += ArrayNumber[i];
-	Arithmetical= sum / len;
- console.log(Arithmetical);
+// function ArithmeticalMean(sum, len) {
+// 	let sum = 0; 
+// 	sum += ArrayNumber[i];
+// 	Arithmetical= sum / len;
+//  console.log(Arithmetical);
 
- for(i=0; i>Arithmetical; i++) {
- 	if( ArrayNumber[i]>Arithmetical) {
- 		console.log(ArrayNumber);
- 	}
- }
-}
+//  for(i=0; i>Arithmetical; i++) {
+//  	if( ArrayNumber[i]>Arithmetical) {
+//  		console.log(ArrayNumber);
+//  	}
+//  }
+// }
 
-getRandomArray(len);
-ArithmeticalMean(sum, len);
-
-
-Задание№3
-
-let len=prompt("Введите длину массива");
-let ArrayNumber=[];
+// getRandomArray(len);
+// ArithmeticalMean(sum, len);
 
 
-function getRandomArray(len) {
-	for(let i=0; i<len; i++)
-		ArrayNumber[i]=Math.round(Math.random()*100);
-	return ArrayNumber;
-}
+// Задание№3
 
-	getRandomArray(len);
-	let c;
-
-function smallestElement() {
-	let a = ArrayNumber[0];
-	let b = ArrayNumber[1];
-array.sort(function(a, b) {
-  	return a - b;
- }
-	console.log(ArrayNumber[0]);
-	console.log(ArrayNumber[1]);
-}
-
-sortFunction(a, b);
-SmallestElement(c);
+// let len=prompt("Введите длину массива");
+// let ArrayNumber=[];
 
 
-// Задание 4
+// function getRandomArray(len) {
+// 	for(let i=0; i<len; i++)
+// 		ArrayNumber[i]=Math.round(Math.random()*100);
+// 	return ArrayNumber;
+// }
+
+// 	getRandomArray(len);
+// 	let c;
+
+// function smallestElement() {
+// 	let a = ArrayNumber[0];
+// 	let b = ArrayNumber[1];
+// array.sort(function(a, b) {
+//   	return a - b;
+//  }
+// 	console.log(ArrayNumber[0]);
+// 	console.log(ArrayNumber[1]);
+// }
+
+// sortFunction(a, b);
+// SmallestElement(c);
 
 
-let len=4;
-let ArrayNumber=[];
+// // Задание 4
 
 
-function getRandomArray(len) {
-	for(let i=0; i<len; i++)
-		ArrayNumber[i]=Math.random();
-	return ArrayNumber;
-}
+// let len=4;
+// let ArrayNumber=[];
 
-	getRandomArray(len);
 
-function Delete() {
-	if (ArrayNumber[i]>0.3) 
-	console.log(ArrayNumber[i])
+// function getRandomArray(len) {
+// 	for(let i=0; i<len; i++)
+// 		ArrayNumber[i]=Math.random();
+// 	return ArrayNumber;
+// }
 
-}
+// 	getRandomArray(len);
 
-Delete(); 
+// function Delete() {
+// 	if (ArrayNumber[i]>0.3) 
+// 	console.log(ArrayNumber[i])
+
+// }
+
+// Delete(); 
